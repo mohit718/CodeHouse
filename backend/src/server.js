5e465548d942d6425a8a2a90c1175207f97c4039
Mohit Soni <mohit18.official@gmail.com>
2026-04-19 14:27:58 +0530

    build script

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,27 @@
 import express from 'express'
 import env from './libs/env.js'
+import path from 'path'
 
 const app = express()
+
+const __project_root = path.join(path.resolve(), '../')
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'API is up & running.', status: 200 });
 })
 
+if (env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__project_root, 'frontend', 'dist')))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__project_root, 'frontend', 'dist', 'index.html'));
+    })
+
+}
+
 const port = env.PORT || 3000
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
+app.listen(port, (error) => {
+    if (error)
+        throw error
+    console.log(`[${env.NODE_ENV}] server running on http://localhost:${port}`)
+})
